c866372a96f1db46e5a8d2707fdc0a3f3b8f27a7
Alymbek Damir Uulu <damiruulu@marshall.edu>
2020-08-18 13:24:25 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-95b7bb716bba738f683e.js"
+    "url": "webpack-runtime-72f11afe5b1faf37ad56.js"
   },
   {
-    "url": "styles.0174863b817e2b4697ef.css"
+    "url": "styles.47f8b0a827b0a94ebe7e.css"
   },
   {
     "url": "styles-c2fe8482057191dca484.js"
@@ -38,29 +38,21 @@ self.__precacheManifest = [
     "url": "framework-85aff51d15c28c7fbe5f.js"
   },
   {
-    "url": "app-dc2710231d7b83fd99e1.js"
+    "url": "app-c296e92b352047a0679d.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-04552f9a779bcd544711.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bcf6ef0a91a0c2b9ba456d499090a7f9"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "c7047792c6f91b88e0d9abc0cd819e92"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "36ca58714340a7310965b42f9049cfe1"
+    "revision": "3667fd0ea72b9164431c6d20862b20ca"
   },
   {
     "url": "polyfill-19a11a2c8a0865319485.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "24d7b4cab75fd7672b791f2590bb74e7"
+    "revision": "c407ce20f01cf26bd52f0442ddf4efba"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -79,12 +71,12 @@ const { NavigationRoute } = workbox.routing
 
 const navigationRoute = new NavigationRoute(async ({ event }) => {
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/web-portfolio`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/web-portfolio/app-dc2710231d7b83fd99e1.js`))) {
+  if (!resources || !(await caches.match(`/app-c296e92b352047a0679d.js`))) {
     return await fetch(event.request)
   }
 
@@ -97,7 +89,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/web-portfolio/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   return await caches.match(offlineShell)
 })
 
